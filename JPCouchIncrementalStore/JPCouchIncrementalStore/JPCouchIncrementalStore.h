97ed1e21f58f0137ba561f97d7e862c782382830
Jamie Pinkham <jamiepinkham@me.com>
2012-11-15 12:22:59 -0500

    created workspace with projects

---
+++ JPCouchIncrementalStore/JPCouchIncrementalStore/JPCouchIncrementalStore.h
@@ -0,0 +1,13 @@
+//
+//  JPCouchIncrementalStore.h
+//  JPCouchIncrementalStore
+//
+//  Created by Jamie Pinkham on 11/15/12.
+//  Copyright (c) 2012 Jamie Pinkham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JPCouchIncrementalStore : NSObject
+
+@end
---
+++ JPCouchIncrementalStore/JPCouchIncrementalStore/JPCouchIncrementalStore.m
@@ -0,0 +1,13 @@
+//
+//  JPCouchIncrementalStore.m
+//  JPCouchIncrementalStore
+//
+//  Created by Jamie Pinkham on 11/15/12.
+//  Copyright (c) 2012 Jamie Pinkham. All rights reserved.
+//
+
+#import "JPCouchIncrementalStore.h"
+
+@implementation JPCouchIncrementalStore
+
+@end
